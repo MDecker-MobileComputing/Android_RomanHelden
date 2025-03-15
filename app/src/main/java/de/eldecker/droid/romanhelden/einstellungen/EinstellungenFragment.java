cab2d68cb2a64c66d83078b88df15d8127a569a8
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-03-15 22:34:27 +0100

    SimpleSummaryProvider fuer Genre

--- app/src/main/java/de/eldecker/droid/romanhelden/einstellungen/EinstellungenFragment.java
+++ app/src/main/java/de/eldecker/droid/romanhelden/einstellungen/EinstellungenFragment.java
@@ -2,6 +2,10 @@ package de.eldecker.droid.romanhelden.einstellungen;
 
 import android.os.Bundle;
 
+import androidx.preference.ListPreference;
+
+import androidx.preference.Preference.SummaryProvider;
+import androidx.preference.ListPreference.SimpleSummaryProvider;
 import androidx.preference.PreferenceFragmentCompat;
 
 import de.eldecker.droid.romanhelden.R;
@@ -14,12 +18,20 @@ import de.eldecker.droid.romanhelden.R;
 public class EinstellungenFragment extends PreferenceFragmentCompat {
 
     /**
-     * Konfiguration für Preferences-Screen laden.
+     * Konfiguration für Preferences-Screen laden und SummaryProvider für Genre-Preference
+     * setzen.
      */
     @Override
     public void onCreatePreferences( Bundle savedInstanceState, String rootKey ) {
 
         setPreferencesFromResource( R.xml.einstellungen, rootKey );
+
+        ListPreference genrePreference = findPreference( "literatur_genre" );
+        if ( genrePreference != null ) {
+
+            SummaryProvider simpleSummaryProvider = SimpleSummaryProvider.getInstance();
+            genrePreference.setSummaryProvider( simpleSummaryProvider );
+        }
     }
 
 }
