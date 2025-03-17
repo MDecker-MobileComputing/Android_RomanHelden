dfbecc26435d8bc04da5da821b39b2a30a1c5de2
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-03-17 09:14:28 +0100

    Update MainActivity.java

--- app/src/main/java/de/eldecker/droid/romanhelden/MainActivity.java
+++ app/src/main/java/de/eldecker/droid/romanhelden/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        super.onCreate(savedInstanceState);
+        super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
 
         _nameTextView = findViewById( R.id.name_textview );
